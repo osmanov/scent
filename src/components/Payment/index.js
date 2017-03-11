9ddc6b6e95277efb6b9d832929357e5369997be9
osmanov <rhenium.osmium@gmail.com>
2017-03-11 15:28:48 +0300

    unit payment fields

--- src/components/Payment/index.js
+++ src/components/Payment/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { IndexLink, Link } from 'react-router'
+import { Field } from 'redux-form'
+import Input from 'components/FormFields/Input'
+import Select from 'components/FormFields/Select'
 import { media } from 'styles/utils'
 
 
@@ -13,7 +15,36 @@ const Wrapper=styled.div`
 
 export const Payment = () => (
   <Wrapper>
-    Payment
+    Secure credit card payment
+    <div>
+      <Field
+        name='cardNumber'
+        component={Input}
+        placeholder='Credit card number' />
+      <Field
+        name='securityCode'
+        component={Input}
+        placeholder='Security code' />
+      <div>
+        <Field name="month" component={Select}>
+          <option value=''></option>
+          <option value='01'>01</option>
+          <option value='02'>02</option>
+          <option value='03'>03</option>
+          <option value='04'>04</option>
+          <option value='05'>05</option>
+          <option value='06'>06</option>
+          <option value='07'>07</option>
+          <option value='08'>08</option>
+        </Field>
+        <Field name="year" component={Select}>
+          <option value=''></option>
+          <option value='2017'>2017</option>
+          <option value='2018'>2018</option>
+          <option value='2019'>2019</option>
+        </Field>
+      </div>
+    </div>
   </Wrapper>
 )
 
